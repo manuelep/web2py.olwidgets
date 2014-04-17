27e2da21056738f56983815652da39c14ad0e0d7
Manuele Pesenti <manuele.pesenti@gmail.com>
2014-04-17 10:46:47 +0200

    minor modifications

--- modules/plugin_olwidgets.py
+++ modules/plugin_olwidgets.py
@@ -4,17 +4,22 @@ from gluon import *
 
 from json import dumps as jsdumps
 
+from geojson import FeatureCollection
+from geojson import loads as gjsloads
+from geojson import dumps as gjsdumps
+from json import dumps as jsdumps
+
 plugin_root = 'static/plugin_olwidgets'
 
 def getUUID():
-    """ Returns random identifier for map tag elements """
+    """ Returns random identifier """
     import uuid
     UUID = str(uuid.uuid1()).split('-')[0]
     return UUID
 
 def injectMap(value, map_id=None, input_id=None, input_name=None,
     input_class='json', widget='initPointMap', edit_mode=False, **kwargs):
-    """ Returns map tag elements I need """
+    """ Returns tags I need to build my map """
 
     if None in (map_id, input_id, ):
         UUID = getUUID()
@@ -46,7 +51,7 @@ def injectMap(value, map_id=None, input_id=None, input_name=None,
         'jQuery( document ).ready( %(widgetname)s("%(myinput)s", %(map_options)s, %(viewmode)s) )' % elements,
         _type = "text/javascript",
     )
-    return SPAN(ValueTag, MapTag, ollib, mylib, callmapscript)
+    return SPAN(ollib, mylib, ValueTag, MapTag, callmapscript)
 
 def map_represent(value, row):
     """ """
@@ -58,10 +63,6 @@ def mapPoint_widget(field, value):
     map_id = "map_%s" % input_id
     return injectMap(value, map_id=map_id, input_id=input_id, input_name=field.name, input_type=field.type, widget='initPointMap', edit_mode=True)
 
-from geojson import FeatureCollection
-from geojson import loads as gjsloads
-from geojson import dumps as gjsdumps
-from json import dumps as jsdumps
 
 def featStore(rows):
     collection = FeatureCollection(map(lambda row: gjsloads(row.localization)['features'][0], rows))
